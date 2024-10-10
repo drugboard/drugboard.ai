1ca43fef544e1d0a684b08cbd1322cf4cdb5f901
sundeeep <sandeepkumar25820@gmail.com>
2024-10-11 02:25:41 +0530

    🚀feature-Appwrite: Appwrite Service is initiated by @sundeeep

---
+++ src/services/db/appwrite/index.js
@@ -0,0 +1,10 @@
+import { Client } from "appwrite";
+import { endpoint, projectId } from "./constants";
+
+const appwriteClient = new Client();
+
+appwriteClient
+  .setEndpoint(`${endpoint}`)
+  .setProject(`${projectId}`);
+  
+export { appwriteClient };
