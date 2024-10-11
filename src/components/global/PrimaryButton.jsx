7251122c3d219d87b6168edb6a117e39fd2ad6a2
sundeeep <sandeepkumar25820@gmail.com>
2024-10-12 03:34:13 +0530

    🗳️Component-Frontend: Custom Primary Btn is written!

---
+++ src/components/global/PrimaryButton.jsx
@@ -0,0 +1,24 @@
+"use client";
+import { Button } from '@nextui-org/button';
+
+const PrimaryButton = ({onClick, className, ariaLable, isIconOnly=false, startContent, endContent, isDisabled=false, size="md", radius="full", variant="solid", isLoading=false, children}) => {
+
+  return (
+    <Button
+        onClick={onClick}
+        aria-label={ariaLable}
+        isIconOnly={isIconOnly}
+        startContent={startContent} 
+        endContent={endContent} 
+        size={size}
+        radius={radius}
+        isLoading={isLoading}
+        isDisabled={isDisabled}
+        variant={variant}
+        color="secondary"
+        className={className}
+    >{children}</Button>
+  )
+}
+
+export default PrimaryButton;
