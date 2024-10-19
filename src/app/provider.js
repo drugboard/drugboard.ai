c1bfc3cccd03bb1c88ef375a9785206fb3c923ef
sundeeep <sandeepkumar25820@gmail.com>
2024-10-19 23:24:37 +0530

    🪶Toaster Config added to ToastContainer

--- src/app/provider.js
+++ src/app/provider.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {NextUIProvider} from "@nextui-org/react";
 import { StyledEngineProvider } from "@mui/material";
-import { ToastContainer} from 'react-toastify';
+import { Slide, ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 // import { WebinarSocketProvider } from "@/services/sockets/WebinarSocket";
 
@@ -14,7 +14,19 @@ function Provider({children}) {
         {/* <WebinarSocketProvider> */}
           {children}
         {/* </WebinarSocketProvider> */}
-        <ToastContainer />  
+        <ToastContainer 
+          position="bottom-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+          transition= {Slide}
+        />  
       </StyledEngineProvider>
     </NextUIProvider>
   );
