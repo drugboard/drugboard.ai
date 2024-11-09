5f99edbba9daab31e3fdbc98a4af69aec72b7f78
sundeeep <sandeepkumar25820@gmail.com>
2024-11-09 23:27:00 +0530

    Primary Button is tweaked!

--- src/components/global/PrimaryButton.jsx
+++ src/components/global/PrimaryButton.jsx
@@ -1,13 +1,14 @@
 "use client";
 import { Button } from '@nextui-org/button';
 
-const PrimaryButton = ({onPress, color="secondary",type, onClick, className, ariaLable, isIconOnly=false, startContent, endContent, isDisabled=false, size="md", radius="full", variant="solid", isLoading=false, children}) => {
+const PrimaryButton = ({fullWidth, onPress, color="secondary",type, onClick, className, ariaLable, isIconOnly=false, startContent, endContent, isDisabled=false, size="md", radius="full", variant="solid", isLoading=false, children}) => {
 
   return (
     <Button
         type={type}
         onPress={onPress}
         onClick={onClick}
+        fullWidth={fullWidth ? fullWidth : false}
         aria-label={ariaLable}
         isIconOnly={isIconOnly}
         startContent={startContent} 
