8ab1b546fda9178114a5600c83ad68b7dc281ea1
sundeeep <sandeepkumar25820@gmail.com>
2024-11-26 12:08:15 +0530

    Started Working on Knowledge Pathways

--- src/app/components/KnowledgePathways.jsx
+++ src/app/components/KnowledgePathways.jsx
@@ -2,12 +2,9 @@
 import React, { useEffect, useState } from 'react'
 import {GraduationCap} from 'lucide-react';
 import ProgressCard from '@/components/ui/KnowledgePathways/ProgressCard';
-import { Tab, Tabs } from '@nextui-org/tabs';
-import { Presentation } from 'lucide-react';
-import { Atom } from 'lucide-react';
+
 
 const KnowledgePathways = () => {
-  const [selectedConferences, setSelectedConferences] = useState("all-conferences");
 
   useEffect(() => {
     // Check if we need to scroll to this section on mount
@@ -41,41 +38,8 @@ const KnowledgePathways = () => {
           </div>
         </div>
 
-       <div className='w-full flex-1 flex items-center justify-between gap-3 p-3'>
-        <div className='flex items-center justify-center bg-white/50 backdrop-blur-lg rounded-3xl h-full w-[60%]'>
-          <ProgressCard />
-        </div>
-        <div className='h-full flex flex-col items-stretch gap-2 w-[40%] rounded-3xl backdrop-blur-lg bg-white/30'>       
-            <Tabs
-              color="secondary" 
-              variant="solid"
-              radius="full"
-              className="mt-2 flex items-center justify-center w-full"
-              size="lg"
-              aria-label="Conferences Listing"
-              selectedKey={selectedConferences}
-              onSelectionChange={setSelectedConferences}
-            >
-              <Tab key={"all-conferences"} title={
-                <div className="flex items-center space-x-2">
-                  <Presentation />
-                  <span className="font-medium text-lg">Conferences</span>
-                </div>
-              }>
-              </Tab>
-              <Tab key={"registered-conferences"} title={
-                <div className="flex items-center space-x-2">
-                  <Atom />
-                  <span className="font-medium text-lg">Registered</span>
-                </div>
-              }>
-              </Tab>
-            </Tabs>
-            <div className='p-2 flex-1 w-full bg-white/80 border border-white rounded-3xl'>
-
-            </div>
-
-        </div>
+       <div className='w-full flex-1 flex items-center justify-between gap-3'>
+        
        </div>
     </section>
   )
