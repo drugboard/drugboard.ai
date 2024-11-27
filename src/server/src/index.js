8b567a2573195ef9a409a4871f9a8eb4529b8016
sundeeep <sandeepkumar25820@gmail.com>
2024-11-27 17:13:40 +0530

    Corrected the mindset of setting the server keep alive

--- src/server/src/index.js
+++ src/server/src/index.js
@@ -1,7 +1,7 @@
 import drugboardServer from './app.js'
 import { PORT } from "./constants.js";
 // import connectMongoDB from "./db/mongodb/index.js";
-
+import fetch from "node-fetch";
 
 const launchDrugboardServer = async () => {
     // try{
@@ -16,6 +16,14 @@ const launchDrugboardServer = async () => {
     //     console.log("Error in starting Drugboard Server: \n", error);
     // }
     try{
+        const keepAlive = () => {
+            fetch('https://drugboard-ai.onrender.com/keep-alive')
+              .then((res) => console.log('Ping successful:', new Date()))
+              .catch((err) => console.error('Ping failed:', err));
+          };
+          
+          // Ping every 30seconds
+          setInterval(keepAlive, 30000);
         const port = PORT || 8001;
         drugboardServer.listen(port, async()=>{
             console.log(`🚀 HTTP Server is running on the port ${port}\n`)
